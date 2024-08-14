a346ef7db09503b710a7de9cc0ad0a186298c0ea
Daniil <daniilkovtun156@gmail.com>
2024-08-14 15:04:31 +0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,9 +3,63 @@ const changeText = document.querySelector(".userlist__but");
 changeText.addEventListener("click", function () {
    if (changeText.textContent === "Subscribe") {
       changeText.textContent = "Unsubscribe";
-   }else {
+   } else {
       changeText.textContent = "Subscribe"
    }
    changeText.classList.toggle("userlist__but--disabled")
+   
 });
 
+
+function darkmode() {
+   const body = document.body
+   const wasDarkmode = localStorage.getItem('darkmode') === 'true'
+
+   localStorage.setItem('darkmode', !wasDarkmode)
+   body.classList.toggle('dark-mode', !wasDarkmode)
+
+}
+
+document.querySelector('.switch__darkmode').addEventListener('click', darkmode)
+
+function onload() {
+   document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode'))
+}
+document.addEventListener('DOMContentLoaded', onload)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const footer__info = userList.querySelectorAll(".footer__info");
+
+// footer__info.forEach((userItem) => {
+//   deleteUser(userItem);
+// });
+
+// const err = document.querySelectorAll(".footer__info");
+
+// err.addEventListener('click', function () {
+//    swal ( "Error" ,  "you have no saved photos" ,  "error" )
+
+//  })
